b3653be94bfa96d7ddb85f8cf8c073961b4d509e
Michael Stenger <michael.stenger@outlook.de>
2023-12-20 13:13:27 +0100

    adding prducer tests

---
+++ PactNetAbstactMessages/ProducerTest.cs
@@ -0,0 +1,57 @@
+﻿namespace PactNetAbstactMessages;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using PactNet;
+using PactNet.Output.Xunit;
+using PactNet.Verifier;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+public class ProducerTest : IDisposable
+{
+    private readonly PactVerifier _pactVerifier;
+
+    public ProducerTest(ITestOutputHelper output)
+    {
+        _pactVerifier = new PactVerifier(new PactVerifierConfig
+        {
+            Outputters = new[] { new XunitOutput(output) },
+            LogLevel = PactLogLevel.Information
+        });
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        _pactVerifier.Dispose();
+    }
+
+    [Fact]
+    public void ShouldSendExpectedMessage()
+    {
+        var pactDir = "../../../pacts/";
+        string pactPath = Path.Combine(pactDir, "Consumer-Producer.json");
+
+        var serializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            TypeNameHandling = TypeNameHandling.Objects,
+            Converters = new List<JsonConverter> { new MatcherWrapperConverter() }
+        };
+
+        _pactVerifier
+            .MessagingProvider("Producer", serializerSettings)
+            .WithProviderMessages(scenarios =>
+            {
+                scenarios.Add(
+                    nameof(Message),
+                    () => new Message(new[] { new DoubleKeyValuePair("Key", 12.3) }));
+            })
+            .WithFileSource(new FileInfo(pactPath))
+            .Verify();
+    }
+}
